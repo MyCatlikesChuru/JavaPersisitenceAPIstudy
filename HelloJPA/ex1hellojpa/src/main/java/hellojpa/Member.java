4a8960b1dafd46c91885ecb2cbde566ac8665153
Lee JaeHyeok <dhfif718@gmail.com>
2022-12-18 21:53:20 +0900

    Hello JPA Complete Setting

---
+++ HelloJPA/ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity // JPA사용하는 엔티티로 인식
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
