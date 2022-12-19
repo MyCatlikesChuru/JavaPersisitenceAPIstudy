04d90f5ecd38df2e4eaf70fd0509410bf5ea4566
Lee JaeHyeok <dhfif718@gmail.com>
2022-12-20 01:32:51 +0900

    JPQL Test

--- HelloJPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ HelloJPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,12 +19,24 @@ public class JpaMain {
 
         try {
 
-            // 객체 생성
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember); // 삭제
 
-            em.persist(member);
+//            findMember.setName("HelloJPA"); // update가 되어짐
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+//
+//            em.persist(findMember);
             tx.commit();
         } catch (Exception e){
             tx.rollback();
