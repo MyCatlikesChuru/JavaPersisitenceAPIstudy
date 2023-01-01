c88195980e43cd07757ed68c560516e35b7e14cb
Lee JaeHyeok <dhfif718@gmail.com>
2023-01-01 16:48:45 +0900

    [JPA] SEQ 전략 추가

--- HelloJPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ HelloJPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -20,19 +20,19 @@ public class JpaMain {
         try {
             Member member1 = new Member();
             member1.setUsername("A");
-//            Member member2 = new Member();
-//            member1.setUsername("B");
-//            Member member3 = new Member();
-//            member1.setUsername("C");
+            Member member2 = new Member();
+            member2.setUsername("B");
+            Member member3 = new Member();
+            member3.setUsername("C");
 
             System.out.println("===============");
             em.persist(member1); // 1,51
-//            em.persist(member2); // memory
-//            em.persist(member3); // memory
+            em.persist(member2); // memory
+            em.persist(member3); // memory
 
             System.out.println("member1 = " + member1.getId());
-//            System.out.println("member2 = " + member2.getId());
-//            System.out.println("member3 = " + member3.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
             System.out.println("===============");
 
 
--- HelloJPA/ex1hellojpa/src/main/java/hellojpa/Member.java
+++ HelloJPA/ex1hellojpa/src/main/java/hellojpa/Member.java
@@ -2,16 +2,17 @@ package hellojpa;
 
 import javax.persistence.*;
 
-@Entity // JPA사용하는 엔티티로 인식]
+@Entity // JPA사용하는 엔티티로 인식
 //@Table(name = "MEMBER")
 @SequenceGenerator(
         name = "MEMBER_SEQ_GENERATOR",
         sequenceName = "MEMBER_SEQ", // 맵핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
+        initialValue = 1, allocationSize = 50) // allocationSize -> call next 한번할때 50개씩 땡겨오도록
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                    generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
