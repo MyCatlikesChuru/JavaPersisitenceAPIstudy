1af9bba8894798cac294456cff6528f393acc93b
Lee JaeHyeok <dhfif718@gmail.com>
2023-01-04 23:18:57 +0900

    [init] JPA Shop

---
+++ JPAShop/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+
+        try {
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ jpabook/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS") // 예약어로 되어있는 DB가 있어 에러가 발생할수 있어 바꿔줌
+public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
