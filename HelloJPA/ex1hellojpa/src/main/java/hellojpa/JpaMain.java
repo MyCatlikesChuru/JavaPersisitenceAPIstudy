8662047784183ce2b2ebfe0d6610f6afb21fb1c9
Lee JaeHyeok <dhfif718@gmail.com>
2023-02-23 21:55:27 +0900

    [feat] 연관관계 맵핑

--- HelloJPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ HelloJPA/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     /*
@@ -25,9 +26,16 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();;
+            em.clear();
+
+            System.out.println("==================");
+
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e){
