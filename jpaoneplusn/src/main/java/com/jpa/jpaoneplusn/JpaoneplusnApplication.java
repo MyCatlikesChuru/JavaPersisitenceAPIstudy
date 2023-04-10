63e505ca09604aed7f8fd7a134d37fee18bc9eb8
Lee JaeHyeok <dhfif718@gmail.com>
2023-04-10 22:39:37 +0900

    [feat] JPA N+1 문제 프로젝트 생성

---
+++ jpaoneplusn/src/main/java/com/jpa/jpaoneplusn/JpaoneplusnApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.jpaoneplusn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaoneplusnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaoneplusnApplication.class, args);
+	}
+
+}
---
+++ jpaoneplusn/src/test/java/com/jpa/jpaoneplusn/JpaoneplusnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.jpaoneplusn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaoneplusnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
